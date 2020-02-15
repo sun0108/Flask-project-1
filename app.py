4fa107de4b1478afcded2a8a71db826b7804c539
Yifei Sun <yifei-sun-1@uiowa.edu>
2020-02-15 12:52:59 +0800

    Draft framework created

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', pageTitle='Flask Server Home Page')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html', pageTitle='About Mike')
+@app.route('/About')
+def About():
+  return render_template('About.html', pageTitle='About')
+
+@app.route('/Estimate')
+def Estimate():
+  return render_template('Estimate.html', pageTitle='Estimate')
 
 if __name__ == '__main__':
     app.run(debug=True)
